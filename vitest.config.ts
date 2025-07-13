f0739b18c4b948a8e6342db6b35306bba2bc4599
Kabiru Haruna <kabiruharuna@Kabirus-MacBook-Pro.local>
2025-07-13 16:20:28 +0100

    update vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,8 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()] as any,
   test: {
     globals: true,
     environment: 'jsdom',
